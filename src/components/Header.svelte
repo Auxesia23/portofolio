b04e28039e6d02ac9d1b2795c519dc35de4aa34c
Fajar Gustiana <fajargustiana2@gmail.com>
2024-10-25 14:08:20 +0700

    adding bottom navbar for mobile screen

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -39,3 +39,23 @@
         </a>
   </div>
 </header>
+
+
+
+<div class="lg:hidden md:hidden fixed z-50 w-[calc(100%-1rem)] h-12 max-w-lg -translate-x-1/2 border border-gray-200 rounded-full bottom-4 left-1/2 bg-black bg-opacity-70">
+    <div class="grid h-full max-w-lg grid-cols-3 mx-auto">
+        <a href="#introPage"  type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-800 group">
+            <i class="fa-solid fa-house"></i>
+            <span>Home</span>
+        </a>
+        <a href="#projects"  type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-800 group">
+           <i class="fa-solid fa-diagram-project"></i>
+           <span>Projects</span>
+        </a>
+        <a href="#about" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-e-full  hover:bg-gray-800 group">
+            <i class="fa-solid fa-user"></i>
+            <span>About</span>
+        </a>
+    </div>
+</div>
+
